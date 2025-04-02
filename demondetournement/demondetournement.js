5be36674481b717e22bffdb02d67f977b4284bb6
Arthur Raynaud <arthur_raynaud@hotmail.com>
2025-04-02 01:20:18 -0700

    Update demondetournement.js

--- demondetournement/demondetournement.js
+++ demondetournement/demondetournement.js
@@ -3,8 +3,8 @@ var links = {
 	"remix.audio": "https://remix.audio/playlist/2953",
 	"YouTube": "https://www.youtube.com/watch?v=lJ_1DWvw-jU&list=PLUq5Z5VhjxM3G9EXQPOAmrBcFro3FwwjS&pp=iAQB8AUB",
 	"Mashup Lovers": "https://www.facebook.com/groups/mashuplovers",
-	"Steff St. Moore": "https://remix.audio/profile/SteffStMoore",
 	"DoM": "https://remix.audio/profile/DoM",
+	"Steff St. Moore": "https://remix.audio/profile/SteffStMoore",
 	"Minor Arth": "https://www.youtube.com/@MinorArth",
 };
 
