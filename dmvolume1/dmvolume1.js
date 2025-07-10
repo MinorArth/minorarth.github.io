c88074ea7d4aa83ca3f9ab9e105c51259e87a27a
Arthur Raynaud <arthur_raynaud@hotmail.com>
2025-07-09 22:45:19 -0700

    Update dmvolume1.js

--- dmvolume1/dmvolume1.js
+++ dmvolume1/dmvolume1.js
@@ -31,7 +31,6 @@ var items = [
 			"Moby - Lift Me Up | 2005",
 			"Depeche Mode - Everything Counts | 1983"
 		],
-		"songTitle": "Everything Lifts Me Up",
 		"artists": "Moby / Depeche Mode"
 	},
 	{
@@ -51,13 +50,12 @@ var items = [
 		"id": 18107,
 		"file": "1211742334_1857274291_1842666095.mp3",
 		"author": "Minor Arth",
-		"title": "Tears For Fears & Depeche Mode - Never Shout Again",
 		"image": "1763003201_1123673197_196675649.jpg",
 		"sources": [
 			"Tears For Fears - Never Shout Again | 1985",
 			"Depeche Mode - Never Let Me Down Again | 1987",
 		],
-		"songTitle": "Never Shout Again",
+		"title": "Never Shout Again",
 		"artists": "Tears For Fears / Depeche Mode",
 		"video": "https://youtu.be/D6RT2ogPu9U"
 	},
@@ -92,13 +90,12 @@ var items = [
 		"id": 51278,
 		"file": "1120979652_755243373_1511517317.mp3",
 		"author": "Minor Arth",
-		"title": "Depeche Mode & Taxi Girl - Photographic Boy | EN | Darc Room mix",
 		"image": "568306564_672843720_1722267045.jpg",
 		"sources": [
 			"Taxi Girl - Cherchez Le Garçon | 1980",
 			"Depeche Mode - Photographic | 1981",
 		],
-		"songTitle": "Photographic Boy",
+		"title": "Photographic Boy",
 		"artists": "Taxi Girl / Depeche Mode",
 		"links": { "Inverse French version": "https://remix.audio/track/51429" },
 		"video": "https://youtu.be/WXMIItxcu3M"
@@ -107,14 +104,13 @@ var items = [
 		"id": 18311,
 		"file": "1036391139_1726185446_1785178290.mp3",
 		"author": "Minor Arth",
-		"title": "Depeche Mode & The Rolling Stones - Gimme Disease",
 		"image": "1601254446_1897991313_212754089.jpg",
 		"sources": [
 			"The Rolling Stones - Gimme Shelter | 1969",
 			"Depeche Mode - Shake The Disease | 1985",
 			"https://youtu.be/ESx-DHaT7hY"
 		],
-		"songTitle": "Gimme Disease",
+		"title": "Gimme Disease",
 		"artists": "The Rolling Stones / Depeche Mode",
 		"video": "https://youtu.be/ESx-DHaT7hY"
 	},
@@ -123,8 +119,7 @@ var items = [
 		"file": "563353933_1226507571_31415805.mp3",
 		"author": "Minor Arth",
 		"artists": "Yello / Depeche Mode",
-		"title": "Yello & Depeche Mode - Lost Again In Silence",
-		"songTitle": "Lost Again In Silence",
+		"title": "Lost Again In Silence",
 		"sources": [
 			"Yello - Lost Again | 1983",
 			"Depeche Mode - Leave In Silence | 1982"
@@ -136,8 +131,7 @@ var items = [
 		"file": "571413028_353465432_238058580.mp3",
 		"author": "Minor Arth",
 		"artists": "Yeah Yeah Yeahs / Depeche Mode",
-		"title": "Depeche Mode & Yeah Yeah Yeahs - The Sun, The Wolf And The Rainfall",
-		"songTitle": "The Sun, The Wolf And The Rainfall",
+		"title": "The Sun, The Wolf And The Rainfall",
 		"sources": [
 			"Yeah Yeah Yeahs - Wolf | 2022",
 			"Depeche Mode - The Sun And The Rainfall | 1982"
@@ -148,26 +142,24 @@ var items = [
 		"id": 51708,
 		"file": "1043273729_684282083_2008796406.mp3",
 		"author": "Minor Arth",
-		"title": "Balance Sans Contrefaçon | EN",
 		"image": "1917008411_1442662475_374910206.jpg",
 		"sources": [
 			"Mylène Farmer - Sans Contrefaçon | 1987",
 			"Depeche Mode - Get the Balance Right | 1982"
 		],
-		"songTitle": "Balance Sans Contrefaçon",
+		"title": "Balance Sans Contrefaçon",
 		"artists": "Mylène Farmer / Depeche Mode"
 	},
 	{
 		"id": 19401,
 		"file": "1992192372_47439495_278507144.mp3",
 		"author": "Minor Arth",
-		"title": "Kraft Mode - People Are Numbers | Kraftwerk & Depeche Mode",
 		"image": "1325777255_834725289_978931676.jpg",
 		"sources": [
 			"Kraftwerk - Numbers | Computer World | 1981",
 			"Depeche Mode - People Are People | 1984",
 		],
-		"songTitle": "People Are Numbers",
+		"title": "People Are Numbers",
 		"artists": "Kraftwerk / Depeche Mode",
 		"video": "https://youtu.be/m8zUtJlff_g"
 	},
@@ -175,14 +167,13 @@ var items = [
 		"id": 53038,
 		"file": "1528258040_172576223_2130344446.mp3",
 		"author": "Minor Arth",
-		"title": "Kavinsky & Depeche Mode - Something To Do In Autodrive",
 		"image": "776688174_1168472281_248581402.jpg",
 		"sources": [
 			"Kavinsky - Testarossa Autodrive | 2007",
 			"Depeche Mode - Something To Do | 1984",
 			"https://youtu.be/FD4xU1Oj8pE"
 		],
-		"songTitle": "Something To Do In Autodrive",
+		"title": "Something To Do In Autodrive",
 		"artists": "Kavinsky / Depeche Mode",
 		"video": "https://youtu.be/FD4xU1Oj8pE"
 	},
@@ -190,23 +181,21 @@ var items = [
 		"id": 59410,
 		"file": "459760669_1107128209_1821248495.mp3",
 		"author": "Minor Arth",
-		"title": "Indochine & Depeche Mode - A Heart At Canary Bay | 80s Mashed vol. 7",
 		"image": "1209501321_242983412_110846060.jpg",
 		"sources": [
 			"Indochine - Canary Bay | 1985",
 			"Depeche Mode - It's Called a Heart | 1985"
 		],
 		"links": { "80s Mashed 7": "https://audioboots.org/Albums/80sMashed7" },
-		"songTitle": "A Heart At Canary Bay",
+		"title": "A Heart At Canary Bay",
 		"artists": "Indochine / Depeche Mode"
 	},
 	{
 		"id": 65880,
 		"file": "771720424_218872985_137092706.mp3",
 		"author": "Minor Arth",
-		"title": "Berlin & Depeche Mode - Take My Lust Away",
 		"artists": "Berlin / Depeche Mode",
-		"songTitle": "Take My Lust Away",
+		"title": "Take My Lust Away",
 		"sources": [
 			"Berlin - Take My Breath Away | 1986",
 			"Depeche Mode - A Question Of Lust | 1986"
@@ -217,13 +206,12 @@ var items = [
 		"id": 18124,
 		"file": "327004920_1108003792_1817607154.mp3",
 		"author": "Minor Arth",
-		"title": "Depeche Mode & Curses - Surrender Celebration",
 		"image": "538570564_1250100579_1044768237.jpg",
 		"sources": [
 			"Curses - Surrender | Jennifer Cardini remix | 2019",
 			"Depeche Mode - Black Celebration | 1986",
 		],
-		"songTitle": "Surrender Celebration",
+		"title": "Surrender Celebration",
 		"artists": "Curses / Depeche Mode",
 		"video": "https://youtu.be/AHcUZkA1zsE"
 	},
@@ -231,13 +219,12 @@ var items = [
 		"id": 34466,
 		"file": "880225046_1980996943_302683165.mp3",
 		"author": "Minor Arth",
-		"title": "Depeche Mode & Chromatics - Love On The Windscreen",
 		"image": "1074895215_811649611_1245966278.jpg",
 		"sources": [
 			"Chromatics - Looking For Love | 2012",
 			"Depeche Mode - Love On The Windscreen | 1986"
 		],
-		"songTitle": "Love On The Windscreen",
+		"title": "Love On The Windscreen",
 		"artists": "Chromatics / Depeche Mode",
 		"video": "https://youtu.be/msqqXcLB5jI"
 	},
@@ -271,36 +258,38 @@ var items = [
 		"id": 19403,
 		"file": "1674492422_1306326625_2003354294.mp3",
 		"author": "Minor Arth",
-		"title": "Kraft Mode - Halo Robots | Kraftwerk & Depeche Mode",
 		"image": "1517760380_736358051_722969622.jpg",
 		"sources": [
 			"Kraftwerk - The Robots | 1978",
 			"Depeche Mode - Halo | 1990"
 		],
-		"songTitle": "Halo Robots",
+		"title": "Halo Robots",
 		"artists": "Kraftwerk / Depeche Mode",
 		"video": "https://youtu.be/pz83tO_aTyM"
 	},
 	{
+		"id": 78083,
+		"file": "341658949_1328178205_1439436524.mp3",
 		"author": "Minor Arth",
+		"image": "1697337059_946053160_98557722.jpg",
 		"artists": "Jean-Michel Jarre / Depeche Mode",
 		"sources": [
 			"Jean-Michel Jarre - Arpegiateur | 1981",
 			"Depeche Mode - Waiting For The Night | 1990"
 		],
-		"songTitle": "Waiting For The Night | Arpegiator mix"
+		"title": "Waiting For The Night | Arpegiator mix",
+		"video": "https://youtu.be/lf-7Yj2zDnw"
 	},
 	{
 		"id": 19836,
 		"file": "232555895_1951216380_819161131.mp3",
 		"author": "Minor Arth",
-		"title": "Depeche Floyd - One Of These Clean Days | Pink Floyd & Depeche Mode",
 		"image": "1355645944_1124524783_860623310.jpg",
 		"sources": [
 			"Pink Floyd - One Of These Days | 1971",
 			"Depeche Mode - Clean + Personal Jesus | 1990",
 		],
-		"songTitle": "One Of These Clean Days",
+		"title": "One Of These Clean Days",
 		"artists": "Pink Floyd / Depeche Mode",
 		"video": "https://youtu.be/q5U8O7WZkrM"
 	},
@@ -314,7 +303,7 @@ var items = [
 			"Sébastien Tellier - L'Amour et la Violence | 2008",
 			"Depeche Mode - Little 15 | 1987"
 		],
-		"songTitle": "Little 15 | Amour & Violence mix",
+		"title": "Little 15 | Amour & Violence mix",
 		"artists": "Sébastien Tellier / Depeche Mode"
 	}
 ];
